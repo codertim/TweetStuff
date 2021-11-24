4e9dc293be82e48b7b371d949a0b10300b3ccf40
Tim F <twforce@yahoo.com>
2021-11-23 18:13:04 -0800

    add user description and name

--- PythonCommandLineTweetReader/tweet_reader.py
+++ PythonCommandLineTweetReader/tweet_reader.py
@@ -8,11 +8,15 @@ def show_followers_count(usr):
   print('Followers count:' + str(usr.followers_count))
 
 
+def show_description(usr):
+  print('User Description: ' + usr.description)
+
+
 def show_friends(usr):
   friends = []
   global current_username
   global user
-  cursor = tweepy.Cursor(api.friends, screen_name=current_username, count=200)
+  cursor = tweepy.Cursor(api.friends, screen_name='coder_tim', count=200)
   for friend in cursor.items(200):
       friends.append(friend.screen_name)
 
@@ -41,13 +45,13 @@ print('Starting')
 # print('settings key:' + settings_mine.access_token)
 
 auth = tweepy.OAuthHandler(settings_mine.consumer_key,
-                          settings_mine.consumer_secret)
+                           settings_mine.consumer_secret)
 
 auth.set_access_token(settings_mine.access_token,
                       settings_mine.access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
-                wait_on_rate_limit_notify=True)
+                 wait_on_rate_limit_notify=True)
 
 current_username = sys.argv[1]
 user = api.get_user(current_username)
@@ -55,27 +59,29 @@ user = api.get_user(current_username)
 
 user_input = ''
 while user_input != 'q':
-    print('\n\n\n\n\n\n\n\n\n\n')
+    print('\n' * 100)
 
     if (user_input == 'a'):
         show_followers_count(user)
     elif (user_input == 'b'):
         show_friends(user)
+    elif (user_input == 'c'):
+        show_description(user)
 
-    print('\n\n')
+    print('\n')
 
-    print('Current username: ', current_username)
+    print('Current username: ', current_username, ' (', user.name, ')')
 
     print('\n\n')
 
     print('a) display followers count')
     print('b) display accounts this user follows')
+    print('c) display description')
     print('q) quit')
     user_input = input()
 
 
 
 print('Done.')
-
 
 
