324aec3049180c3cc668f06e9a19edbd487bf6f0
Tim F <twforce@yahoo.com>
2021-12-06 13:00:08 -0800

    show most recent tweet; remove duplicate code

--- PythonCommandLineTweetReader/tweet_reader.py
+++ PythonCommandLineTweetReader/tweet_reader.py
@@ -1,7 +1,11 @@
 
-import tweepy
+import os
+import platform
+import re
 import settings_mine
 import sys
+import tweepy
+
 
 
 def show_followers_count(usr):
@@ -16,7 +20,7 @@ def show_friends(usr):
   friends = []
   global current_username
   global user
-  cursor = tweepy.Cursor(api.friends, screen_name='coder_tim', count=200)
+  cursor = tweepy.Cursor(api.friends, screen_name=usr.screen_name, count=200)
   for friend in cursor.items(200):
       friends.append(friend.screen_name)
 
@@ -37,6 +41,29 @@ def show_friends(usr):
     print('new friend screen name:', new_friend_screen_name)
     current_username = new_friend_screen_name
     user = api.get_user(current_username)
+
+
+
+def show_most_recent_tweet(twitter_user):
+  print(f'Most recent tweet for {twitter_user.screen_name}:')
+  print('  ', twitter_user.status.text)
+
+  # check for link
+  link_match = re.search('http\S*', twitter_user.status.text)
+  if link_match != None:
+    print('1) open link in browser')
+    print('<enter> to go back to the main menu')
+    recent_tweet_input = input()
+
+    if recent_tweet_input == '1':
+      url_link = link_match.group(0)
+      os_name = platform.system()
+      print(f'os_name: {os_name}')
+      if os_name == 'Windows':
+        os.system(f'start msedge {url_link}')
+      else:
+        print('OS not currently supported')
+
 
 
 ############### main part ##################
@@ -66,7 +93,7 @@ user = api.get_user(current_username)
 
 user_input = ''
 while user_input != 'q':
-    print('\n' * 100)
+    print('\n' * 200)
 
     if (user_input == 'a'):
         show_followers_count(user)
@@ -74,6 +101,8 @@ while user_input != 'q':
         show_friends(user)
     elif (user_input == 'c'):
         show_description(user)
+    elif (user_input == 'd'):
+        show_most_recent_tweet(user)
 
     print('\n')
 
@@ -84,93 +113,7 @@ while user_input != 'q':
     print('a) display followers count')
     print('b) display accounts this user follows')
     print('c) display description')
-    print('q) quit')
-    user_input = input()
-
-
-
-print('Done.')
-
-
-
-import tweepy
-import settings_mine
-import sys
-
-
-def show_followers_count(usr):
-  print('Followers count:' + str(usr.followers_count))
-
-
-def show_description(usr):
-  print('User Description: ' + usr.description)
-
-
-def show_friends(usr):
-  friends = []
-  global current_username
-  global user
-  cursor = tweepy.Cursor(api.friends, screen_name='coder_tim', count=200)
-  for friend in cursor.items(200):
-      friends.append(friend.screen_name)
-
-  friends = sorted(friends, key=lambda k: k.lower())
-  print('Friends:', friends)
-  # print('Friends: ', ', '.join(sorted(friends, key=lambda s: s.lower())))
-  i = 0
-  for friend in friends:
-      i = i + 1
-      print(i, ') ', friend, sep='', end=(' ' * 3))
-
-  print('\n\nSelect user or press <return> to go back to main menu')
-  friend_input = input()
-  if friend_input != '':
-    idx_new_friend = int(friend_input) -1
-    print('idx_new_friend:', idx_new_friend)
-    new_friend_screen_name = friends[idx_new_friend]
-    print('new friend screen name:', new_friend_screen_name)
-    current_username = new_friend_screen_name
-    user = api.get_user(current_username)
-
-
-
-print('Starting')
-
-# print('settings key:' + settings_mine.access_token)
-
-auth = tweepy.OAuthHandler(settings_mine.consumer_key,
-                           settings_mine.consumer_secret)
-
-auth.set_access_token(settings_mine.access_token,
-                      settings_mine.access_token_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True,
-                 wait_on_rate_limit_notify=True)
-
-current_username = sys.argv[1]
-user = api.get_user(current_username)
-
-
-user_input = ''
-while user_input != 'q':
-    print('\n' * 100)
-
-    if (user_input == 'a'):
-        show_followers_count(user)
-    elif (user_input == 'b'):
-        show_friends(user)
-    elif (user_input == 'c'):
-        show_description(user)
-
-    print('\n')
-
-    print('Current username: ', current_username, ' (', user.name, ')')
-
-    print('\n\n')
-
-    print('a) display followers count')
-    print('b) display accounts this user follows')
-    print('c) display description')
+    print('d) display most recent tweet')
     print('q) quit')
     user_input = input()
 
